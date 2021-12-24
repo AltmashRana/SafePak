0ceb49675c3f485a8cf55c22837aa9a295cc6264
AltmashRana <altmashrana.303@gmail.com>
2021-12-24 11:29:53 +0500

    abc

--- app/src/main/java/com/example/safepak/frontend/chatAdapters/ChatlistItem.kt
+++ app/src/main/java/com/example/safepak/frontend/chatAdapters/ChatlistItem.kt
@@ -3,16 +3,3 @@ package com.example.safepak.frontend.chatAdapters
 import com.example.safepak.data.User
 import com.xwray.groupie.Item
 import com.xwray.groupie.GroupieViewHolder
-
-
-abstract class ChatlistItem(val user: User): Item<GroupieViewHolder>() {
-//    override fun bind(viewHolder: GroupieViewHolder, position: Int) {
-//        viewHolder.itemView.username_textview_new_message.text = user.username
-//
-//        Picasso.get().load(user.profileImageUrl).into(viewHolder.itemView.imageview_new_message)
-//    }
-//
-//    override fun getLayout(): Int {
-//        return R.layout.user_row_new_message
-//    }
-}
