d317444bc88711fa9e7de033fc1b976133f8e5f5
AltmashRana <altmashrana.303@gmail.com>
2021-12-24 12:07:45 +0500

    Added git ignore

--- app/src/main/java/com/example/safepak/frontend/chatAdapters/ChatlistItem.kt
+++ app/src/main/java/com/example/safepak/frontend/chatAdapters/ChatlistItem.kt
@@ -1,5 +1,23 @@
 package com.example.safepak.frontend.chatAdapters
 
+import com.bumptech.glide.Glide
+import com.example.safepak.R
 import com.example.safepak.data.User
-import com.xwray.groupie.Item
-import com.xwray.groupie.GroupieViewHolder
+import com.example.safepak.logic.session.StorageSession
+import com.xwray.groupie.kotlinandroidextensions.Item
+import com.xwray.groupie.kotlinandroidextensions.GroupieViewHolder
+
+abstract class ChatlistItem(val user: User): Item() {
+//    override fun bind(viewHolder: GroupieViewHolder, position: Int) {
+//        viewHolder.chat
+//
+//        Glide.with(viewHolder.itemView)
+//            .load(user.img?.let { StorageSession.pathToReference(it) })
+//            .placeholder(R.drawable.empty_dp)
+//            .into(viewHolder.dp)
+//    }
+//
+//    override fun getLayout(): Int {
+//        return R.layout.chatlist_item
+//    }
+}
